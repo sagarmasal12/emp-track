be19df454300298f87db9ea1cc89a8999e5033d5
sagarmasal12 <sagarmasal12@gmail.com>
2025-05-03 11:03:10 +0530

    pdfchange

---
+++ src/app/print-pdf/print-pdf.component.ts
@@ -0,0 +1,330 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { formatDate } from '@angular/common';
+import { ButtonModule } from 'primeng/button';
+import { StepperModule } from 'primeng/stepper';
+import {AbstractControl, FormArray, FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import { EmployeeService } from '../core/services/employee/employee.service'; 
+import { CommonModule } from '@angular/common'; 
+import { AutoErrorDirective } from '../directives/auto-error.directive';
+import { ActivatedRoute } from '@angular/router';
+import { DatePickerModule } from 'primeng/datepicker';
+
+@Component({
+  selector: 'app-print-pdf',
+  imports: [ButtonModule,StepperModule,ReactiveFormsModule,CommonModule,DatePickerModule],
+  templateUrl: './print-pdf.component.html',
+  styleUrl: './print-pdf.component.css'
+})
+export class PrintPdfComponent {
+
+  personalDetail:any[]=[];
+  formData:any;
+  employeeService=inject(EmployeeService);
+  employeeForm: FormGroup = new FormGroup({});
+  step = 1;
+  masterData: any;
+  companyList: any []= [];
+  validationErrors: string[] = [];
+  editId:number = 0;
+  isApiSucces: boolean = false;
+  date:Date|undefined;
+  minDate:Date | undefined;
+  empData:any;
+  constructor(private fb: FormBuilder,private activateRoute: ActivatedRoute) {
+    this.activateRoute.params.subscribe((res:any)=>{
+      this.editId  = res.id;
+      if(this.editId !=0) {
+        this.getEmpBYId();
+      } 
+    })
+    
+  };
+
+  onStateChange(){
+    if(this.employeeForm &&  this.employeeForm.controls['contactFamily']) {
+      const state =  this.employeeForm.controls['contactFamily'] as FormGroup;
+    
+      if( state.controls['state']) {
+        const stateVal = state.controls['state'].value;
+        return this.masterData['district'].filter((m:any)=>m.parentMasterId == stateVal)
+      } else {
+        return  []
+      }
+      
+    }
+    
+  }
+  nextStep() {
+    const currentGroup = this.getStepGroup();
+    if (currentGroup.valid) {
+      this.validationErrors = [];
+      this.step++;
+    } else {
+      this.validationErrors = this.getValidationErrors(currentGroup);
+      currentGroup.markAllAsTouched();
+    }
+  }
+
+  getEmpBYId( ){
+    this.employeeService.getEmployeeById(this.editId).subscribe({
+      next: (response) => {
+        this.empData =  response;
+     
+      debugger;
+      setTimeout(()=>{
+        this.isApiSucces = true;
+      },1000)
+      
+      },
+      error: (err) => {
+        alert(JSON.stringify(err.error))
+      }
+      
+    });
+  }
+
+  loadMaster( ){
+    this.employeeService.getMasterData().subscribe({
+      next: (response) => {
+       this.masterData =  response
+      },
+      error: (err) => {
+        alert(JSON.stringify(err.error))
+      }
+      
+    });
+  }
+  GetCompanies( ){
+    this.employeeService.GetCompanies().subscribe({
+      next: (response) => {
+       this.companyList =  response
+      },
+      error: (err) => {
+        alert(JSON.stringify(err.error))
+      }
+      
+    });
+  }
+  
+  
+
+  prevStep() {
+    if (this.step > 1) this.step--;
+  }
+
+  getStepGroup(): FormGroup {
+    switch (this.step) {
+      case 1: return this.employeeForm.get('personalInfo') as FormGroup;
+      case 2: return this.employeeForm.get('contactFamily') as FormGroup;
+      case 3: return this.employeeForm.get('qualificationExpe') as FormGroup;
+      case 4: return this.employeeForm.get('additionalInfo') as FormGroup;
+      case 5: return this.employeeForm.get('salaryPayroll') as FormGroup;
+      case 6: return this.employeeForm.get('statutory') as FormGroup;
+      default: return this.employeeForm;
+    }
+  }
+
+  getValidationErrors(group: FormGroup): string[] {
+    const errors: string[] = [];
+    Object.keys(group.controls).forEach(key => {
+      const control = group.get(key);
+      if (control && control.invalid) {
+        errors.push(`${key} is required.`);
+      }
+    });
+    return errors;
+  }
+
+  isFieldInvalid(groupName: string, fieldName: string): boolean {
+    const control = this.employeeForm.get(`${groupName}.${fieldName}`);
+    return control?.invalid && control?.touched ? true : false;
+  }
+  get personalInfoForm(): FormGroup {
+    return this.employeeForm.get('personalInfo') as FormGroup;
+  }
+  get contactFamilyForm():FormGroup{
+    return this.employeeForm.get('contactFamily') as FormGroup;
+  }
+  // Getter to access FormArray
+  get familyMembers(): FormArray {
+    return this.employeeForm.get('contactFamily.employeeFamilyMembers') as FormArray;
+  }
+  get qualificationExpeForm():FormGroup{
+    return this.employeeForm.get('qualificationExpe') as FormGroup;
+  }
+  get experienceDetails(): FormArray {
+    return this.qualificationExpeForm.get('employeeExperiences') as FormArray;
+  }
+  get additionalInfoForm(): FormGroup {
+    return this.employeeForm.get('additionalInfo') as FormGroup;
+  }
+  get salaryPayrollForm(): FormGroup {
+    return this.employeeForm.get('salaryPayroll') as FormGroup;
+  }
+  get statutoryForm():FormGroup{
+    return this.employeeForm.get('statutory') as FormGroup;
+  }
+    
+ ngOnInit(): void {
+  //this.disablePersonalInfoValidators();
+ // this.addExperience();  // Initialize with one experience row
+  this.loadMaster();
+  this.GetCompanies();
+  const storedData = localStorage.getItem('formData');
+   if (storedData) {
+    console.log('Retrieved from localStorage:', JSON.parse(storedData));
+   } else {
+    console.log('No data found in localStorage');
+   }
+  }
+ 
+ // Method to create a new FormGroup for family member
+  createFamilyMember(familyMember: any = null): FormGroup {
+    debugger;
+   return this.fb.group({
+     familyMemberId: [familyMember?.familyMemberId || 0],
+     employeeId: [familyMember?.employeeId || 0],
+     name: [familyMember?.name || '11', Validators.required],
+     yearOfBirth: [familyMember?.yearOfBirth || '11', Validators.required],
+     relation: [familyMember?.relation || '11', Validators.required]
+   });
+  }
+
+  addFamilyMember() {
+    console.log('Add Family Member clicked!'); 
+    const familyGroup = this.createFamilyMember();
+    this.familyMembers.push(familyGroup);
+    
+    console.log('Family Members:', this.familyMembers.value);  // Check the added members
+  }
+  removeFamilyMember(index: number) {
+    this.familyMembers.removeAt(index);
+  }
+
+  createExperience(): FormGroup {
+    return this.fb.group({
+      employerName: [''],
+      postHeld: [''],
+      fromDate: [''],
+      toDate: [''],
+      reasonForLeaving: ['']
+    });
+  }
+
+  patchExperience(data:any): FormGroup {
+    return this.fb.group({
+      experienceId: [data.experienceId],
+      employeeId: [data.employeeId],
+      employerName: [data.employerName],
+      postHeld: [data.postHeld],
+      fromDate: [ formatDate(data.fromDate,'yyyy-MM-dd','en')],
+      toDate: [formatDate(data.toDate,'yyyy-MM-dd','en')],
+      reasonForLeaving: [data.reasonForLeaving]
+    });
+  }
+
+  addExperience(): void {
+    this.experienceDetails.push(this.createExperience());
+  }
+   // Remove experience row
+   removeExperience(index: number): void {
+    if (this.experienceDetails.length > 1) {
+      this.experienceDetails.removeAt(index);
+    }
+  }
+
+
+ disablePersonalInfoValidators() {
+   const personalInfoGroup = this.employeeForm.get('personalInfo') as FormGroup;
+   const contactFamilyGroup = this.employeeForm.get('contactFamily') as FormGroup;
+   const qualificationExpeGroup = this.employeeForm.get('qualificationExpe') as FormGroup;
+   const additionalInfoGroup = this.employeeForm.get('additionalInfo') as FormGroup; 
+   const salaryPayrollGroup = this.employeeForm.get('salaryPayroll') as FormGroup;  //  Disable Step 5
+ 
+   if (personalInfoGroup) {
+      personalInfoGroup.disable();   //  Completely disable Step 2
+      console.log('Step 2 disabled');
+   }
+ 
+   if (contactFamilyGroup) {
+      contactFamilyGroup.disable();  //  Completely disable Step 3
+      console.log('Step 3 disabled');
+   }
+ 
+   if (qualificationExpeGroup) {
+      qualificationExpeGroup.disable();  //  Optionally disable Step 3 as well
+      console.log('Step 3 qualification section disabled');
+   }
+   if (additionalInfoGroup) {    //  Disabling Step 4
+      additionalInfoGroup.disable();
+      console.log('Step 4 disabled');
+    }
+    if (salaryPayrollGroup) {    
+      salaryPayrollGroup.disable();  //  Disable Step 5
+      console.log('Step 5 disabled');
+    }
+
+  }
+  onSave() {
+    // if (this.employeeForm.invalid) {
+    //   return;
+    // }
+    const formData = this.employeeForm.value;
+    const payload = {
+       
+      ...formData.personalInfo,
+      ...formData.contactFamily,
+      ...formData.qualificationExpe,
+      ...formData.additionalInfo,
+      ...formData.salaryPayroll,
+      ...formData.statutory,
+  
+      employeeFamilyMembers: formData.contactFamily.employeeFamilyMembers.map((member: any) => ({
+        familyMemberId: member.familyMemberId, // Default for new records
+        employeeId: member.employeeId, // Fetch if needed
+        name: member.name,
+        yearOfBirth: member.yearOfBirth,
+        relation: member.relation,
+      })),
+  
+      employeeExperiences: formData.qualificationExpe.employeeExperiences.map((exp: any) => ({
+        experienceId: exp.experienceId, // Default
+        employeeId: exp.employeeId, // Fetch if needed
+        employerName: exp.employerName,
+        postHeld: exp.postHeld,
+        fromDate: exp.fromDate,
+        toDate: exp.toDate,
+        reasonForLeaving: exp.reasonForLeaving,
+      }))
+    };
+  
+    console.log('Prepared Payload:', payload);
+    payload.employeeId = this.editId;
+    this.employeeService.updateEmployee(payload).subscribe({
+      next: (response) => {
+        console.log('Success:', response);
+        alert("Employee Update Success")
+      },
+      error: (err) => {
+        alert(JSON.stringify(err.error))
+      }
+      
+    });
+  }
+  onReset() {
+    this.employeeForm.reset();
+    this.disablePersonalInfoValidators();
+    localStorage.removeItem('formData'); // Clear localStorage if needed
+  }
+  print() {
+    const printContents = document.getElementById('print-section')?.innerHTML;
+    const originalContents = document.body.innerHTML;
+
+    if (printContents) {
+      document.body.innerHTML = printContents;
+      window.print();
+      document.body.innerHTML = originalContents;
+      window.location.reload(); // optional: reload to restore state
+    }
+  }
+}
